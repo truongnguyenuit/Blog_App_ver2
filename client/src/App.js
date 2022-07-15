856de2ddeddb2d6a21384aa100f8b6e31ecbfb6d
Nguyễn Xuân Trường <truongnguyenuit@gmail.com>
2022-07-15 10:58:31 +0700

    Create folder

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import { DatePicker } from 'antd';
+
+function App() {
+  return (
+    <div className="font-bold">
+      <p>truongvjpvvvv</p>
+      <DatePicker></DatePicker>
+    </div>
+  );
+}
+
+export default App;
